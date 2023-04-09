c0b06d87ed617f2992b84e07fe1de1d8c67c8012
kwilkes314 <kendallwilkes.kw@gmail.com>
2023-04-09 18:27:07 -0500

    week 3 homework

---
+++ .vscode/week1 homework/readme.md
@@ -0,0 +1,45 @@
+# Above-Average Code (Capstone Project Weeks 1-4)
+
+## Definition Statement
+I want to build an app that connects minorities, underrepresented people, and people who lack the financial means to fund their tech dream. With bootcamps, government programs, and private people and entities who need motivated tech individuals.
+
+##Definition of Audience
+Minorities, Underrepresented people in tech, People who can't afford college or bootcamps on their own.
+
+## Elevator Pitch
+Hi, my name is Kendall Wilkes. I am 27 years old. I have a 4-year-old son who makes me greater in every way. Tech, video games, and muay thai are some of my greatest loves. I chose Savvy because of their stellar reputation and also my early interaction with staff during the prework. I chose my capstone because I believe someone's out there trying to break into tech right now and I haven't created the website. The problem my capstone solves is that it helps non-traditional people to land roles in tech. This project was created using HTML CSS, JS.
+
+## What do I want the functionality of each page to be?
+
+Home- welcome to my page access other pages sign in
+
+Login- login to my website
+
+Contact:Contact us questions help etc
+
+About: information about business
+
+Resources: statistics on software engineer salary links
+
+To bootcamp page.
+
+Bootcamp: page has info on different types of bootcamps cost, ratings and more.
+
+Videos: page has tutorials podcasts testimonials and more
+Blog: a link to company ran blog.
+
+## What is the purpose of my application?
+
+The purpose of my application is to connect people who want to get a boot camp but lack the funds to go. Though still has the drive.
+
+## What business problem or real-world problem am I going to be solving?
+
+Connecting motivated people who want to break into tech with employers.
+
+## Story Map(Mural)
+[Mural!](https://app.mural.co/t/capstone2524/m/capstone2524/1679936968517/cbf1e90834ccb0f93d8e129a7830a283917bbaf8?sender=uac231e01ca441a47f5ae9024)
+
+##
+
+
+
--- week3/week3 homework.js
+++ week3/week3 homework.js
@@ -1,27 +1,30 @@
 //Create an array of pizzaToppings with at least four different toppings
-const pizzaToppings = ["ham", "pepperoni", "bacon", "canadian bacon"];
+let pizzaToppings = ["ham", "pepperoni", "bacon", "canadian bacon"];
 
 //Create a greetCustomer function that prints a message that welcomes a guest, then informs them of the available toppings by looping over pizzaToppings (don't worry about perfect grammar here yet, i.e. "a, b, and c", see Bonus Challenge #9)
 //i.e. "Welcome to Pizza House, our toppings are: a, b, c, ..."
 
 
-let greeting = "welcome to Pizza House our toppings are: ";
+function greetCustomer() {
+  console.log('Welcome to Pizza House');
 
-for (let pizza of pizzaToppings){
-greeting += `${pizza}, `;
+for (let topping of pizzaToppings){
+  console.log(topping);
+
 }
-console.log(greeting);
 
-
+}
 //Create a getPizzaOrder function that
 //has the parameters size, crust, and an indefinite amount of toppings as inputs
 //prints the order, i.e. "One large thick crust pizza with x, y, z, ... coming up!"
 //outputs a list with the size, crust, and toppings
 function getPizzaOrder(size, crust, ...toppings) {
-  console.log('One' + ' ' + size + ' ' + crust + ' ' +' pizza with' + ' ' + toppings + ' ' + '...coming up!');}
-  console.log(getPizzaOrder("large", "thin", "ham"));
+  console.log(`One ${size} ${crust} crust pizza with ${toppings}. Coming right up!`)
+  return [size, crust, toppings]; //outputting list of size, crust, and toppings
 
-;
+};
+
+
 
 
 
@@ -30,21 +33,35 @@ function getPizzaOrder(size, crust, ...toppings) {
 
 //Create a preparePizza function that
 //has an array as its parameter with three items: a size, a crust, and a list of toppings
-function preparePizza(large,thin,ham) {
- console.log('...Cooking pizza...');
- let servePizza = preparePizza;
- return servePizza;
-
-}
-console.log(preparePizza('large','thin','ham'));
-
 //prints something like "...Cooking pizza..."
 //outputs a pizza Object with appropriate key-value pairs for size, crust, and toppings
 //Create a servePizza function that
 
+function preparePizza([size,crust,...toppings]) {
+ console.log('...Cooking pizza...');
+ let pizza = {
+  size: size,
+  crust: crust,
+  toppings: toppings
+
+
+ }
+ return pizza;
+
+}
+function servePizza(pizza) {
+  console.log(`Order up! ${pizza.size}, ${pizza.crust}, ${pizza.toppings}`)
+  return pizza;
+}
+
+//Create a servePizza function that
 //has a parameter of a pizza Object
 //logs a message that the pizza is ready and repeats the order, i.e. "Order up! Here's your large thick crust pizza with x, y, z, ... Enjoy!"
 //outputs the same pizza Object that was passed
 
 
+
 //Call each function and (starting with preparePizza) use the returned value from the previous function as its input
+greetCustomer();
+servePizza(preparePizza(getPizzaOrder("Large", "Stuffed crust", "turkey", "spinach", "onions")));
+
