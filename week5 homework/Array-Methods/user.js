dbad79269942141c6fda6434cb1054c9963ee1f3
kwilkes314 <kendallwilkes.kw@gmail.com>
2023-04-20 11:21:20 -0500

    week 5 homework

---
+++ week5 homework/Array-Methods/user.js
@@ -0,0 +1,93 @@
+[{"completed": true
+},
+{
+  "userId": 2,
+  "id": 36,
+  "title": "excepturi deleniti adipisci voluptatem et neque optio illum ad",
+  "completed": true
+},
+{
+  "userId": 2,
+  "id": 37,
+  "title": "sunt cum tempora",
+  "completed": false
+},
+{
+  "userId": 2,
+  "id": 38,
+  "title": "totam quia non",
+  "completed": false
+},
+{
+  "userId": 2,
+  "id": 39,
+  "title": "doloremque quibusdam asperiores libero corrupti illum qui omnis",
+  "completed": false
+},
+{
+  "userId": 2,
+  "id": 40,
+  "title": "totam atque quo nesciunt",
+  "completed": true
+},
+{
+  "userId": 3,
+  "id": 41,
+  "title": "aliquid amet impedit consequatur aspernatur placeat eaque fugiat suscipit",
+  "completed": false
+},
+{
+  "userId": 3,
+  "id": 42,
+  "title": "rerum perferendis error quia ut eveniet",
+  "completed": false
+},
+{
+  "userId": 3,
+  "id": 43,
+  "title": "tempore ut sint quis recusandae",
+  "completed": true
+},
+{
+  "userId": 3,
+  "id": 44,
+  "title": "cum debitis quis accusamus doloremque ipsa natus sapiente omnis",
+  "completed": true
+},
+{
+  "userId": 3,
+  "id": 45,
+  "title": "velit soluta adipisci molestias reiciendis harum",
+  "completed": false
+},
+{
+  "userId": 3,
+  "id": 46,
+  "title": "vel voluptatem repellat nihil placeat corporis",
+  "completed": false
+},
+{
+  "userId": 3,
+  "id": 47,
+  "title": "nam qui rerum fugiat accusamus",
+  "completed": false
+},
+{
+  "userId": 3,
+  "id": 48,
+  "title": "sit reprehenderit omnis quia",
+  "completed": false
+},
+{
+  "userId": 3,
+  "id": 49,
+  "title": "ut necessitatibus aut maiores debitis officia blanditiis velit et",
+  "completed": false
+},
+{
+  "userId": 3,
+  "id": 50,
+  "title": "cupiditate necessitatibus ullam aut quis dolor voluptate",
+  "completed": true
+}
+]
